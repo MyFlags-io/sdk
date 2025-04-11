9072188a85c211779868225463621d5021bd2ce2
Michał Makowski <michal.makowski97@gmail.com>
2025-04-11 23:00:10 +0200

    added featureflag component

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,4 @@
 export { MyFlagsProvider } from "./provider";
 export { default as useFlag } from "./hooks/useFlag";
 export { default as useFlags } from "./hooks/useFlags";
+export { FeatureFlag } from "./components/FeatureFlag";
